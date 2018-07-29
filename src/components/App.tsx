350f5d98eaa23eaca691e719fd66a9ac07f8fc39
Chris Lee <chris.leeshanok@gmail.com>
2018-07-29 15:45:11 -0400

    Initial commit using CRA w/ TS, SASS

---
+++ src/components/App.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import './App.scss'
+
+interface iProps {
+
+}
+
+interface iState {
+
+}
+
+class App extends React.PureComponent<iProps, iState> {
+  public render() {
+    return (
+      <div className="App">
+        <nav>
+
+        </nav>
+
+        <section className="header">
+
+        </section>
+
+        <section className="story">
+
+        </section>
+
+        <section className="rsvp">
+
+        </section>
+
+        <section className="wedding-party">
+
+        </section>
+
+        <section className="faq">
+
+        </section>
+      </div>
+    )
+  }
+}
+
+export default App
