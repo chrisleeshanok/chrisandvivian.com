6728c763f18efd6142455300174d631ee8bb696c
Chris Lee-Shanok <chris.leeshanok@gmail.com>
2019-08-25 00:23:13 -0400

    Overhaul

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -20,28 +20,32 @@ export default class App extends React.PureComponent<iProps, iState> {
         <div className="vertical-wrapper">
           <div className="horizontal-wrapper">
             <div className="photo">
+              <div className="ribbon">
+              Thank You!
+              </div>
               <img className="thankYouPhoto" src={thankyouBg} title="thank you"/>
             </div>
-            <div className="title">
-              Thank You!
-            </div>
             <p className="description">
-              We are underway onto the next chapter of our lives together.<br/>
-              We'll always look back to the big day and all the love and support you showed us. We are so lucky to have such amazing friends and family.<br/>
-              We hope you all had a great night. From the bottom of our hearts, THANK YOU for making it extra special.<br/>
-              <br/>
+              Our wedding has come and gone, and we have started an exciting chapter of our lives together.
+            </p>
+            <p className="description">
+              We feel blessed to have had you all celebrating the big day with us. We are so lucky to have such amazing friends and family.<br/>
+              From the bottom of our hearts, thank you for making our day so special.
+            </p>
+            <p className="description">
               Sincerely,<br/>
               <br/>
               Chris &amp; Vivian
             </p>
+            <hr/>
             <p className="description">
               You can find all the photos from the wedding in the gallery linked below. Feel free to download and post them to your own social media channels.
             </p>
-            <p>
+            <div className="gallery">
               <button onClick={this.handleOpenGallery}>
                 Gallery
               </button>
-            </p>
+            </div>
           </div>
         </div>
       </div>
